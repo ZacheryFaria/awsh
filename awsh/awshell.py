73b7a777de3c036cd532b4feefb7d23605c8591d
Zachery Faria <zacheryfaria@gmail.com>
2020-06-15 22:45:29 -0700

    Removeds scripts and use execve to use ssh now

--- awsh/awshell.py
+++ awsh/awshell.py
@@ -1,5 +1,4 @@
 from awsh.aws_connector import AWSHConnector
-from awsh.exit_statuses import SSH_EXIT_OK, EXIT_FAILURE, EXIT_OK
 import click
 import subprocess
 import time
@@ -11,9 +10,37 @@ import json
 user_home = str(Path.home())
 config_file = os.path.join(user_home, '.awshell')
 
+def which_ssh():
+    global_path = os.environ['PATH']
+
+    if os.name == 'nt':
+        ssh_name = 'ssh.exe'
+    else:
+        ssh_name = 'ssh'
+
+    if os.path.isfile(ssh_name) and os.access(ssh_name, os.X_OK):
+        return program
+
+    for path in global_path.split(';'):
+        fpath = os.path.join(path, ssh_name)
+        if os.path.isfile(fpath) and os.access(fpath, os.X_OK):
+            return fpath
+
+    return False
+
 
 def enter_ssh(ip, key):
-    print(f"{ip}~{key}", end='')
+    ssh_loc = which_ssh()
+
+    if ssh_loc is False:
+        print("ssh doesn't appear to be installed on your machine. Check your path?")
+        exit(1)
+
+    if os.name == 'nt':
+        sts = os.spawnv(os.P_WAIT, ssh_loc, ['ssh', '-i', key, f'ubuntu@{ip}'])
+        exit(sts)
+    else:
+        os.execle(ssh_loc, 'ssh', '-i', key, f"ubuntu@{ip}", os.environ)
 
 
 def exec_script(instance_ips, key, file):
@@ -70,46 +97,44 @@ def main(configure, key, ls, instance: str, copy, files):
         with open(config_file, 'w') as file:
             json.dump(aws_credentials, file)
 
-        exit(EXIT_OK)
+        exit(0)
 
     aws = AWSHConnector(config_file)
 
     if ls:
         print(aws.instances_string(), file=sys.stderr)
-        exit(EXIT_FAILURE)
+        exit(1)
 
     if instance is None:
         print("Error: Missing argument 'INSTANCE'.")
-        exit(EXIT_FAILURE)
+        exit(1)
 
     if key is None:
         if "AWSH_KEY" in os.environ:
             key = os.environ["AWSH_KEY"]
         else:
             print("ssh key not provided, please set AWSH_KEY environment variable or supply -i parameter.")
-            exit(EXIT_FAILURE)
+            exit(1)
 
     ips = aws.get_ip(instance)
 
     if len(ips) == 0:
         print(f"{instance} does not exist!")
         print(aws.instances_string())
-        exit(EXIT_FAILURE)
+        exit(1)
 
     if len(files) == 0:
         if len(ips) > 1:
             print("Error: cannot ssh with multiple ips. Please specify your instance.")
-            exit(EXIT_FAILURE)
+            exit(1)
         else:
             enter_ssh(ips[0], key)
-            exit(SSH_EXIT_OK)
 
     for file in files:
         copy_file(ips, key, file)
 
     if not copy:
         exec_script(ips, key, files[0])
-        exit(EXIT_OK)
 
 
 if __name__ == '__main__':
